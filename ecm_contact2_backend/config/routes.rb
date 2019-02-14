bfc421df506581bdbf78129238d7669a4998f7ca
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-02-14 17:08:25 +0100

    Ecm::Contact::Backend: Add show route.

--- ecm_contact2_backend/config/routes.rb
+++ ecm_contact2_backend/config/routes.rb
@@ -1,5 +1,5 @@
 Ecm::Contact::Backend::Engine.routes.draw do
-  resources :contact_requests, only: [:index, :edit, :update, :delete] do
+  resources :contact_requests, only: [:index, :edit, :update, :delete, :show] do
     post :notify, on: :member
   end
 
