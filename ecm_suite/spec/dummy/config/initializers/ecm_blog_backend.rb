94c3af53afabf0aef34c2625a1391044c3f5f35a
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2019-03-01 15:55:13 +0100

    wip

--- ecm_suite/spec/dummy/config/initializers/ecm_blog_backend.rb
+++ ecm_suite/spec/dummy/config/initializers/ecm_blog_backend.rb
@@ -19,13 +19,6 @@ Ecm::Blog::Backend.configure do |config|
   config.registered_services = -> {[
   ]}
 
-  # Method to call on creator for display. Can be a symbol,
-  # string or Proc.
-  #
-  # default: config.creator_label_method = :to_s
-  #
-  config.creator_label_method = :to_s
-
   # This proc is used in context of the PostsController to retrieve the current
   # user to userstamp created/updated posts.
   #
